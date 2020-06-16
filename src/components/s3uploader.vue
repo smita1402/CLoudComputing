cd8f472fffa02d7e3419f9d08374c85d46a0337e
Manish Parida <manishparida123@gmail.com>
2020-06-16 15:35:54 +0530

    added extra html tags

--- src/components/s3uploader.vue
+++ src/components/s3uploader.vue
@@ -13,7 +13,9 @@
     </div>
     <h2 v-if="uploadURL">Success! Video uploaded to:</h2>
     <a :href="uploadURL">{{ uploadURL }}</a>
+    <div>
     <a href="https://v2-alpha.d3v123wpfzmfwj.amplifyapp.com/">Visit S3 Explorer!</a>
+    </div>
   </div>
 </template>
 
